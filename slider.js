965ad8401d9dcf2ac80bc3000e923afc0143a93e
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-10 20:10:06 +0900

    js imag btn slide

--- slider.js
+++ slider.js
@@ -18,3 +18,34 @@ function slide () {
     }
 slide();
 setInterval(slide, 3000);
+
+const sliders = document.querySelectorAll('.sliders > img');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+
+
+let current = 0;
+
+
+function showSlider(n) {
+    for (let i=0; i < sliders.length; i++) {
+        sliders[i].style.display = 'none';
+    }
+    sliders[n].style.display = 'block';
+}
+
+function prevSlide() {
+    if (current >0) current -= 1;
+    else current = sliders.length -1;
+    showSlider(current);
+}
+
+function nextSlide () {
+    if (current < sliders.length-1) current +=1;
+    else current = 0;
+    showSlider(current);
+}
+
+showSlider(current);
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
